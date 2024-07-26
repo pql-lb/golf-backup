43bf270d3634083884f5a51ec6969208157947aa
pql-lb <lbaker1594@hotmail.com>
2024-07-26 19:56:22 +0100

    test pwa

--- next.config.js
+++ next.config.js
@@ -1,30 +1,11 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require('next-pwa/cache');
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true, 
-  skipWaiting: true,
-});
+const withPWA = require('next-pwa');
+
 const nextConfig = withPWA({
-    experimental: {
-        serverActions: {
-          allowedOrigins: ['*'],
-        },
-    },
-    webpack: (config) => {
-      config.resolve.alias.canvas = false;
-      return config;
-    },
-    images: {
-      remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: 'images.ctfassets.net',
-              port: '',
-          
-          },
-      ],
-  },
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
   pwa: {
     dest: 'public',
     runtimeCaching: [
@@ -48,9 +29,24 @@ const nextConfig = withPWA({
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+    ],
+  },
 });
 
 module.exports = nextConfig;
-
-
-
