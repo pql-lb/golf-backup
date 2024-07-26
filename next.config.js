00bf2734c31f4cc8b905e87f0bbc526bc79bff92
pql-lb <lbaker1594@hotmail.com>
2024-07-26 20:33:07 +0100

    test

--- next.config.js
+++ next.config.js
@@ -4,26 +4,7 @@ const isDev = process.env.NODE_ENV !== "production";
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: isDev,
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/chat-golf-project.vercel.app\/.*$/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'api-cache',
-        },
-      },
-      {
-        urlPattern: /^https:\/\/chat-golf-project.vercel.app\/.*$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'static-assets',
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 Days
-          },
-        },
-      },
-    ],
+    
 });
 const nextConfig = withPWA({
     experimental: {
