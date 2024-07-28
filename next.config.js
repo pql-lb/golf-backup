f4efe57d0313944f17dd4175527c2e8820338b47
pql-lb <lbaker1594@hotmail.com>
2024-07-28 22:31:31 +0100

    pwa udpates

--- next.config.js
+++ next.config.js
@@ -4,7 +4,22 @@ const isDev = process.env.NODE_ENV !== "production";
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: isDev,
-  skipWaiting: true
+  skipWaiting: true,
+  buildExcludes: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+        if (
+            asset.name.startsWith("server/") ||
+            asset.name.match(/^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/)
+        ) {
+            return true;
+        }
+        if (isDev && !asset.name.startsWith("static/runtime/")) {
+            return true;
+        }
+        return false;
+    }
+  ],
 });
 const nextConfig = withPWA({
     experimental: {
