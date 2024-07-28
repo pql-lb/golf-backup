12937d43fc58ed4c0ddf41c6ff211c5636f34e8c
pql-lb <lbaker1594@hotmail.com>
2024-07-28 22:37:15 +0100

    styling fix

--- middleware.tsx
+++ middleware.tsx
@@ -12,5 +12,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/((?!api/submit-form|_next/static|favicon.ico|images).*)"],
+    matcher: ["/((?!api|_next/static|favicon.ico|images).*)"],
 };
